bb66f40437d529fec9ae33a4cbfba025ce1c7060
Lorenzo Coppi <lorenzo.coppi@cys4.com>
2023-11-07 19:48:20 +0100

    fix: UIOptions fonts used

--- src/main/java/com/cys4/sensitivediscoverer/model/UIOptions.java
+++ src/main/java/com/cys4/sensitivediscoverer/model/UIOptions.java
@@ -3,7 +3,7 @@ package com.cys4.sensitivediscoverer.model;
 import java.awt.*;
 
 public final class UIOptions {
-    public static final Font H1_FONT = new Font("Lucida Grande", Font.BOLD, 16);
-    public static final Font H2_FONT = new Font("Lucida Grande", Font.BOLD, 14);
+    public static final Font H1_FONT = new Font("SansSerif", Font.BOLD, 16);
+    public static final Font H2_FONT = new Font("SansSerif", Font.BOLD, 14);
     public static final Color ACCENT_COLOR = new Color(255, 102, 51);
 }
