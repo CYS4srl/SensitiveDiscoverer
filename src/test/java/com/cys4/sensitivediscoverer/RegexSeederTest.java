0506f3df30be6632678a173734bc18e6344369fb
Lorenzo Coppi <lorenzo.coppi@cys4.com>
2024-01-08 11:55:24 +0100

    feat: at least one positive match

--- src/test/java/com/cys4/sensitivediscoverer/RegexSeederTest.java
+++ src/test/java/com/cys4/sensitivediscoverer/RegexSeederTest.java
@@ -1,29 +1,34 @@
 package com.cys4.sensitivediscoverer;
 
 import com.cys4.sensitivediscoverer.model.RegexEntity;
+import org.assertj.core.api.Condition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RegexSeederTest {
+    private final Condition<String> positiveMatch = new Condition<>(s -> !(s.startsWith("!")), "positive match");
 
     private void testRegexList(List<RegexEntity> regexList) {
         assertThat(regexList)
                 .isNotEmpty()
-                .allSatisfy(regex -> assertThat(regex.getTests())
-                        .as("%s", regex.getDescription())
-                        .isNotEmpty()
-                        .allSatisfy(s -> {
-                            if (s.startsWith("!"))
-                                assertThat(s.substring(1))
-                                        .isNotEmpty()
-                                        .doesNotContainPattern(regex.getRegexCompiled());
-                            else
-                                assertThat(s)
-                                        .isNotEmpty()
-                                        .containsPattern(regex.getRegexCompiled());
-                        }));
+                .allSatisfy(regex -> {
+                    assertThat(regex.getTests())
+                            .as("%s", regex.getDescription())
+                            .isNotEmpty()
+                            .haveAtLeastOne(positiveMatch)
+                            .allSatisfy(s -> {
+                                if (s.startsWith("!"))
+                                    assertThat(s.substring(1))
+                                            .isNotEmpty()
+                                            .doesNotContainPattern(regex.getRegexCompiled());
+                                else
+                                    assertThat(s)
+                                            .isNotEmpty()
+                                            .containsPattern(regex.getRegexCompiled());
+                            });
+                });
     }
 
     @org.junit.jupiter.api.Test
