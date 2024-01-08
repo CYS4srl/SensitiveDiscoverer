d3080276681f97de78ab6fae63324749e9f1d0f6
Lorenzo Coppi <lorenzo.coppi@cys4.com>
2024-01-08 11:34:31 +0100

    feat: does not match tests

--- src/test/java/com/cys4/sensitivediscoverer/RegexSeederTest.java
+++ src/test/java/com/cys4/sensitivediscoverer/RegexSeederTest.java
@@ -8,29 +8,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RegexSeederTest {
 
-    @org.junit.jupiter.api.Test
-    void getGeneralRegexes() {
-        List<RegexEntity> regexList = RegexSeeder.getGeneralRegexes();
+    private void testRegexList(List<RegexEntity> regexList) {
         assertThat(regexList)
                 .isNotEmpty()
                 .allSatisfy(regex -> assertThat(regex.getTests())
                         .as("%s", regex.getDescription())
                         .isNotEmpty()
-                        .allSatisfy(s -> assertThat(s)
-                                .isNotEmpty()
-                                .containsPattern(regex.getRegexCompiled())));
+                        .allSatisfy(s -> {
+                            if (s.startsWith("!"))
+                                assertThat(s.substring(1))
+                                        .isNotEmpty()
+                                        .doesNotContainPattern(regex.getRegexCompiled());
+                            else
+                                assertThat(s)
+                                        .isNotEmpty()
+                                        .containsPattern(regex.getRegexCompiled());
+                        }));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGeneralRegexes() {
+        testRegexList(RegexSeeder.getGeneralRegexes());
     }
 
     @org.junit.jupiter.api.Test
     void getExtensionRegexes() {
-        List<RegexEntity> regexList = RegexSeeder.getExtensionRegexes();
-        assertThat(regexList)
-                .isNotEmpty()
-                .allSatisfy(regex -> assertThat(regex.getTests())
-                        .as("%s", regex.getDescription())
-                        .isNotEmpty()
-                        .allSatisfy(s -> assertThat(s)
-                                .isNotEmpty()
-                                .containsPattern(regex.getRegexCompiled())));
+        testRegexList(RegexSeeder.getExtensionRegexes());
     }
 }
