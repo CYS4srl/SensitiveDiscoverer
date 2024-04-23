59e54ebd2f9e71ae4922e47f072624cfc1ec75e4
Lorenzo Coppi <lorenzo.coppi@cys4.com>
2024-04-23 17:06:44 +0200

    feat: add missing tests for RegexScannerOptionsTest

--- src/test/java/com/cys4/sensitivediscoverer/mock/PreferencesMock.java
+++ src/test/java/com/cys4/sensitivediscoverer/mock/PreferencesMock.java
@@ -3,34 +3,47 @@ package com.cys4.sensitivediscoverer.mock;
 import burp.api.montoya.persistence.Preferences;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Set;
 
 public class PreferencesMock implements Preferences {
+    HashMap<String, String> preferences = new HashMap<>();
+
     @Override
     public String getString(String key) {
-        return null;
+        return preferences.get(key);
     }
 
     @Override
     public void setString(String key, String value) {
+        preferences.put(key, value);
     }
 
     @Override
     public Boolean getBoolean(String key) {
-        return null;
+        String value = preferences.get(key);
+        return Objects.isNull(value) ? null : Boolean.parseBoolean(value);
     }
 
     @Override
     public void setBoolean(String key, boolean value) {
+        preferences.put(key, String.valueOf(value));
     }
 
     @Override
     public Integer getInteger(String key) {
-        return null;
+        try {
+            String value = preferences.get(key);
+            return Objects.isNull(value) ? null : Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     @Override
     public void setInteger(String key, int value) {
+        preferences.put(key, String.valueOf(value));
     }
 
     @Override
